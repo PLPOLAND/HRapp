7ec967f219356cf24d1dfa1622976de79662e6ce
Marek Pałdyna <marekpaldyna@wp.pl>
2019-10-13 10:37:20 +0200

    Init

---
+++ src/main/java/HRapp/MAJ/MajApplication.java
@@ -0,0 +1,13 @@
+package HRapp.MAJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajApplication.class, args);
+	}
+
+}
